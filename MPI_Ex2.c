50a85a749cfab8de4f7869ea3e2a4118f748bf82
LucasCta <lucas.ct.araujo@gmail.com>
2022-12-10 01:37:12 -0300

    Adding Exercise 2 first implementation

---
+++ MPI_Ex2.c
@@ -0,0 +1,29 @@
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc,char *argv[]) {
+
+   int numtasks, rank, dest, source, count, tag=1;
+   int inmsg, outmsg=1000;
+   MPI_Status Stat;
+
+   MPI_Init(&argc,&argv);
+   MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
+   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+   if (rank == 0){
+      FILE *fp; fp = fopen(argv[1], "w");
+      for (source = 0; source < (numtasks-1)*100; source++){
+           MPI_Recv(&inmsg, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &Stat); 
+           fprintf(fp, "Recieved %d from Task %d \n", inmsg, Stat.MPI_SOURCE);
+      } fclose(fp);
+   } else {
+      for (source = 0; source < 100; source++){
+          outmsg = rand()%10000;
+          MPI_Send(&outmsg, 1, MPI_INT, 0, tag, MPI_COMM_WORLD); 
+      }
+   }
+   MPI_Finalize();
+
+} 
