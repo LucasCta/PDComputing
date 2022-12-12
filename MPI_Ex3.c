dc6d74fb422809c9699a5633ac79f2fd7f66042a
LucasCta <lucas.ct.araujo@gmail.com>
2022-12-12 02:36:17 -0300

    Ex3 structure

--- MPI_Ex3.c
+++ MPI_Ex3.c
@@ -1,6 +1,7 @@
 #include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main(int argc,char *argv[]) {
 
@@ -13,16 +14,24 @@ int main(int argc,char *argv[]) {
    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
    if (rank == 0){
+   
       FILE *fp; fp = fopen(argv[1], "w");
+      clock_t before = clock();
+      
       for (source = 0; source < (numtasks-1)*100; source++){
            MPI_Recv(&inmsg, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &Stat); 
            fprintf(fp, "Recieved %d from Task %d \n", inmsg, Stat.MPI_SOURCE);
-      } fclose(fp);
+      } 
+      
+      double final_time = (double) (clock() - before) / CLOCKS_PER_SEC; 
+      printf("Execution Time: %lf\n",final_time);
+      fclose(fp);
+      
+      
    } else {
-      for (source = 0; source < 100; source++){
-          outmsg = rand()%10000;
-          MPI_Send(&outmsg, 1, MPI_INT, 0, tag, MPI_COMM_WORLD); 
-      }
+   
+   
+   
    }
    
    MPI_Finalize();
