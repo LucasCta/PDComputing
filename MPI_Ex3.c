72c89e289ab151f025ecca5aec958d6842f43191
LucasCta <lucas.ct.araujo@gmail.com>
2022-12-12 23:31:16 -0300

    WIP Ex3 and Makefile

--- MPI_Ex3.c
+++ MPI_Ex3.c
@@ -5,7 +5,7 @@
 
 int main(int argc,char *argv[]) {
 
-   int numtasks, rank, dest, source, count, tag=1;
+   int numtasks, rank, dest, source, count, tag = 0;
    int inmsg, outmsg;
    MPI_Status Stat;
 
@@ -15,23 +15,50 @@ int main(int argc,char *argv[]) {
 
    if (rank == 0){
    
-      FILE *fp; fp = fopen(argv[1], "w");
       clock_t before = clock();
       
-      for (source = 0; source < (numtasks-1)*100; source++){
-           MPI_Recv(&inmsg, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &Stat); 
-           fprintf(fp, "Recieved %d from Task %d \n", inmsg, Stat.MPI_SOURCE);
-      } 
+      while (tag++ < argv[4]){
+        //Recebe os vetores
+        //Multiplica conteudo por 2
+        //Envia os vetores
+        MPI_Recv(vet1, 1, sizeof(vet1), 0, tag, MPI_COMM_WORLD, &Stat);
+        MPI_Recv(vet2, 1, sizeof(vet2), 0, tag, MPI_COMM_WORLD, &Stat); 
+        MPI_Recv(vet3, 1, sizeof(vet3), 0, tag, MPI_COMM_WORLD, &Stat);
+        printf("%lf",vet1[1]);
+      }
       
       double final_time = (double) (clock() - before) / CLOCKS_PER_SEC; 
       printf("Execution Time: %lf\n",final_time);
-      fclose(fp);
-      
       
    } else {
-   
-   
-   
+    
+     int i;
+     vet1 = (double*) malloc(argv[1] * sizeof(double));
+     for (i = 0; i < argv[1]; i++) 
+        vet1[i] = 1;
+     vet2 = (double*) malloc(argv[2] * sizeof(double));
+     for (i = 0; i < argv[2]; i++) 
+        vet2[i] = 1;
+     vet3 = (double*) malloc(argv[3] * sizeof(double));
+     for (i = 0; i < atoi(argv[3]); i++) 
+        vet3[i] = 1;  
+         
+     while (tag++ < argv[4]){
+        //Multiplica conteudo por 2
+        for (i = 0; i < atoi(argv[1]); i++) vet1[i] *= 2;
+        for (i = 0; i < atoi(argv[2]); i++) vet2[i] *= 2;
+        for (i = 0; i < atoi(argv[3]); i++) vet3[i] *= 2; 
+        //Envia os vetores
+        MPI_Send(vet1, 1, sizeof(vet1), 1, tag, MPI_COMM_WORLD); 
+        MPI_Send(vet2, 1, sizeof(vet2), 1, tag, MPI_COMM_WORLD); 
+        MPI_Send(vet3, 1, sizeof(vet3), 1, tag, MPI_COMM_WORLD); 
+        //free(vet1); free(vet2); free(vet3);
+        //Recebe os vetores
+        MPI_Recv(vet1, 1, sizeof(vet1), 0, tag, MPI_COMM_WORLD, &Stat);
+        MPI_Recv(vet2, 1, sizeof(vet2), 0, tag, MPI_COMM_WORLD, &Stat); 
+        MPI_Recv(vet3, 1, sizeof(vet3), 0, tag, MPI_COMM_WORLD, &Stat);
+     } 
+     
    }
    
    MPI_Finalize();
