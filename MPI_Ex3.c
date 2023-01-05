d9b81f67c9a3f063aad462f2388c7bcd4e36d48a
LucasCta <lucas.ct.araujo@gmail.com>
2023-01-05 03:44:12 -0300

    writing own atoi function

--- MPI_Ex3.c
+++ MPI_Ex3.c
@@ -1,21 +1,39 @@
 #include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+
+int power(int x, int y){
+    if (y == 0) return 1;
+    int temp = x;
+    while (--y > 0){
+        x *= temp;
+    } return x;
+}
+
+int atoint(char * arg){
+   int i, value, sizeArgv;
+   value = 0; sizeArgv = 0;
+   for (i = 0; arg[i] != '\0'; i++)
+      sizeArgv += 1;
+   for (i = 0; i < sizeArgv; i++)
+      value +=  (arg[i] - '0') * power(10,sizeArgv-i-1);
+   return value;
+}
 
 int main(int argc,char *argv[]) {
 
    int numtasks, rank, dest, source, count, tag = 0;
    int inmsg, outmsg, cont, i;
+   int tamanho; int trocas;
    double * vet;
    MPI_Status Stat;
 
    MPI_Init(&argc,&argv);
    MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-   
-   int tamanho = atoi(argv[1]); int trocas = atoi(argv[2]);
-   
+
+   tamanho = atoint(argv[1]); trocas = atoint(argv[2]);
+
    vet = (double*) malloc(tamanho * sizeof(double*));
    for (i = 0; i < tamanho; i++) 
       vet[i] = -1;
