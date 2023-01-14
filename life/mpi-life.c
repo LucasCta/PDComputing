c5b618a55cea510b471aae47f440953672228fb0
LucasCta <lucas.ct.araujo@gmail.com>
2023-01-14 01:59:26 -0300

    Fixing identation

--- life/mpi-life.c
+++ life/mpi-life.c
@@ -12,26 +12,26 @@ bool ** alloc_2d_bool (int rows, int cols) {
 }
 
 int countAdjacency (bool ** board, int sizeRows, int sizeColumn, int i, int j) {
-	int	m, n, count=0;
-	for (m = i-1; m < i+2; m++){
-		for (n = j-1; n < j+2; n++) {
-	        if (n < 0 || n > sizeColumn) continue;
-	        count += board[m][n];
-	    }
-	} count -= board[i][j];
-	return count;
+    int    m, n, count=0;
+    for (m = i-1; m < i+2; m++){
+        for (n = j-1; n < j+2; n++) {
+            if (n < 0 || n > sizeColumn) continue;
+            count += board[m][n];
+        }
+    } count -= board[i][j];
+    return count;
 }
 
 void mpi_life (bool ** board, bool ** nextBoard, int sizeRows, int sizeColumn) {
-	int	i, j, a;
-	for (i=1; i<sizeRows-1; i++) {
-		for (j=0; j<sizeColumn; j++) {
-		    a = countAdjacency(board, sizeRows, sizeColumn, i, j);
-			if (a == 2) nextBoard[i][j] = board[i][j];
-			if (a == 3) nextBoard[i][j] = 1;
-			if (a < 2) nextBoard[i][j] = 0;
-			if (a > 3) nextBoard[i][j] = 0;
-		}
+    int    i, j, a;
+    for (i=1; i<sizeRows-1; i++) {
+        for (j=0; j<sizeColumn; j++) {
+            a = countAdjacency(board, sizeRows, sizeColumn, i, j);
+            if (a == 2) nextBoard[i][j] = board[i][j];
+            if (a == 3) nextBoard[i][j] = 1;
+            if (a < 2) nextBoard[i][j] = 0;
+            if (a > 3) nextBoard[i][j] = 0;
+        }
     }
 }
 
@@ -46,27 +46,27 @@ int main (int argc,char *argv[]) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
    
     int i,j;   
-	int size, steps, numRows;
-    	
+    int size, steps, numRows;
+        
     if (rank == 0) {
        
         //Read Board
-	    FILE *f; f = stdin;
+        FILE *f; f = stdin;
         fscanf(f,"%d %d",&size,&steps);
         
         // Allocate Board
-	    bool ** board = alloc_2d_bool(size+2,size);
-	    for (j=0; j<size; j++){
-	        board[0][j] = false;
-	        board[size+1][j] = false;
-	    } 
-	    
+        bool ** board = alloc_2d_bool(size+2,size);
+        for (j=0; j<size; j++){
+            board[0][j] = false;
+            board[size+1][j] = false;
+        } 
+        
         // Get Board from File
         char *s = (char *) malloc(size+10);
         for (i=1; i<size+1; i++) {
-	        fgets (s, size+10, f);
-	        for (j=0; j<size; j++)
-		        board[i][j] = (s[j] == 'x');
+            fgets (s, size+10, f);
+            for (j=0; j<size; j++)
+                board[i][j] = (s[j] == 'x');
         } free(s); fclose(f);
         
         // Sends Basic Information
@@ -89,9 +89,9 @@ int main (int argc,char *argv[]) {
         
         // Print Final Board
         for (i=0; i<size+2; i++) {
-	        for (j=0; j<size; j++) 
-		        printf ("%c", board[i][j] ? 'O' : '-');
-	        printf ("\n");
+            for (j=0; j<size; j++) 
+                printf ("%c", board[i][j] ? 'O' : '-');
+            printf ("\n");
         } printf("\n\n");
 
         //Free Allocated Space for Board
@@ -118,35 +118,35 @@ int main (int argc,char *argv[]) {
         MPI_Recv(&board[0][0], size*(numRows+2), MPI_C_BOOL, 0, tag, MPI_COMM_WORLD, &Stat);
         
         int s;
-	    for (s=0; s<steps; s++) {
-	    
-	        //Play Game of Life
-	        mpi_life(board,nextBoard,numRows+2,size);
-	        tmp = nextBoard; nextBoard = board; board = tmp;
-	        
-	        //Trades Edges
-	        if (numtasks > 2){
-	            if (rank == numtasks-1) {
-	                MPI_Recv(&board[0][0], size, MPI_C_BOOL, rank-1, tag, MPI_COMM_WORLD, &Stat);
-	                MPI_Send(&board[1][0], size, MPI_C_BOOL, rank-1, tag, MPI_COMM_WORLD); 
-	            } else if (rank == 1) {
-	                MPI_Send(&board[numRows][0], size, MPI_C_BOOL, rank+1, tag, MPI_COMM_WORLD);  
-	                MPI_Recv(&board[numRows+1][0], size, MPI_C_BOOL, rank+1, tag, MPI_COMM_WORLD, &Stat);
-	            } else {  
-	                MPI_Recv(&board[0][0], size, MPI_C_BOOL, rank-1, tag, MPI_COMM_WORLD, &Stat);
-	                MPI_Send(&board[numRows][0], size, MPI_C_BOOL, rank+1, tag, MPI_COMM_WORLD); 
-	                MPI_Send(&board[1][0], size, MPI_C_BOOL, rank-1, tag, MPI_COMM_WORLD); 
-	                MPI_Recv(&board[numRows+1][0], size, MPI_C_BOOL, rank+1, tag, MPI_COMM_WORLD, &Stat);
-	            }
+        for (s=0; s<steps; s++) {
+        
+            //Play Game of Life
+            mpi_life(board,nextBoard,numRows+2,size);
+            tmp = nextBoard; nextBoard = board; board = tmp;
+            
+            //Trades Edges
+            if (numtasks > 2){
+                if (rank == numtasks-1) {
+                    MPI_Recv(&board[0][0], size, MPI_C_BOOL, rank-1, tag, MPI_COMM_WORLD, &Stat);
+                    MPI_Send(&board[1][0], size, MPI_C_BOOL, rank-1, tag, MPI_COMM_WORLD); 
+                } else if (rank == 1) {
+                    MPI_Send(&board[numRows][0], size, MPI_C_BOOL, rank+1, tag, MPI_COMM_WORLD);  
+                    MPI_Recv(&board[numRows+1][0], size, MPI_C_BOOL, rank+1, tag, MPI_COMM_WORLD, &Stat);
+                } else {  
+                    MPI_Recv(&board[0][0], size, MPI_C_BOOL, rank-1, tag, MPI_COMM_WORLD, &Stat);
+                    MPI_Send(&board[numRows][0], size, MPI_C_BOOL, rank+1, tag, MPI_COMM_WORLD); 
+                    MPI_Send(&board[1][0], size, MPI_C_BOOL, rank-1, tag, MPI_COMM_WORLD); 
+                    MPI_Recv(&board[numRows+1][0], size, MPI_C_BOOL, rank+1, tag, MPI_COMM_WORLD, &Stat);
+                }
             }
             
             #ifdef DEBUG
             // Print Board
             printf("\n\n=== Rank: %d Step: %d ===\n\n",rank,s);
             for (i=0; i<numRows+2; i++) {
-	            for (j=0; j<size; j++) 
-		            printf ("%c", board[i][j] ? 'O' : '-');
-	            printf ("\n");
+                for (j=0; j<size; j++) 
+                    printf ("%c", board[i][j] ? 'O' : '-');
+                printf ("\n");
             } 
             printf("\n========================\n\n");
             #endif
@@ -165,6 +165,6 @@ int main (int argc,char *argv[]) {
     }
   
     MPI_Finalize();
-	
+    
 }
-	
+    
